73c3ef3239680695c52e787ee5db4a9e4bf42bcf
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2018-05-06 10:30:01 -0700

    Recipe component updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ShoppingComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
     RecipeBookComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipesComponent,
     RecipeDetailComponent
   ],
   imports: [
