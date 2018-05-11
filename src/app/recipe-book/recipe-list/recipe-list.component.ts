0454700ae53986f2911542278e8358779ff6b5cc
Joseph Childress <josephchildress@josephs-mbp.ds.ad.adp.com>
2018-05-11 15:53:37 -0700

    Updates including adding the Recipe model and building out the recipe list component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+//IMPORT RECIPE DATA MODEL
+import { Recipe } from '../recipe-book.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe','Only a test recipe','https://c.pxhere.com/photos/fa/d4/eat_vegetables_food_kohl_nutrition_onions_healthy_meatless-424515.jpg!d')
+  ];
   constructor() { }
 
   ngOnInit() {
